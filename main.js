e076f3a831b5297ce3f593728ffe1be5745d6927
Terrence Ho <terrenceho.books@gmail.com>
2018-10-12 17:13:00 -0700

    Add Devx Club Rankings manually

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 console.log("Hi");
 
-console.log("DevX is the best club at UCLA");
+console.log("DevX is the 1st best club at UCLA");
+console.log("DevX is the 2nd best club at UCLA");
+console.log("DevX is the 3rd best club at UCLA");
+console.log("DevX is the 4th best club at UCLA");
+console.log("DevX is the 5th best club at UCLA");
+
